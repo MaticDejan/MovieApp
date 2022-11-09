2c9725e8d23d094b43e71dbde9f58eb2091f120d
MaticDejan <matic.dejan.andrei@gmail.com>
2022-11-09 12:55:08 +0200

    style

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -8,13 +8,5 @@ export const environment = {
         storageBucket: "movie-app-48659.appspot.com",
         messagingSenderId: "471573096423",
         appId: "1:471573096423:web:b8a609b3eced982a7fff42"
-
-        //apiKey: "AIzaSyCUWphHIqavXuOYLccXGOZ9zNnRiOa2PRs",
-        //authDomain: "movie-review-app-7fa54.firebaseapp.com",
-        //projectId: "movie-review-app-7fa54",
-        //storageBucket: "movie-review-app-7fa54.appspot.com",
-        //messagingSenderId: "119853979794",
-        //appId: "1:119853979794:web:63fc8e8361b8761a30ae4b",
-        //databaseURL: "https://movie-review-app-7fa54-default-rtdb.europe-west1.firebasedatabase.app/"
     }
 };
